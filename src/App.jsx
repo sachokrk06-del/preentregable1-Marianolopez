7110ed7de42b59a88249ced8555eceb64951ffa7
mariano lopez <marianito1111@hotmail.com>
2026-03-10 17:36:07 -0300

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 
 import './App.css'
-
+import Titulo from "./components/Titulo"
 function App() {
 
 
   return (
     <div>
+      <Titulo/>
       <h1>hoala mundo</h1>
     </div>
   )
---
+++ src/components/Titulo.jsx
@@ -0,0 +1,10 @@
+const Titulo = () => {
+
+    return(
+        <div>
+            <h1>Titulo</h1>
+        </div>
+    )
+}
+
+export default Titulo
